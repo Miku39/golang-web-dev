9260a12cefe25b4d3af573e9775e0c48691a9ab5
Miku Sano <miku39.sano@gmail.com>
2023-01-22 14:24:17 +0900

    add: section6 hands-on

---
+++ section6/09_hands-on/main.go
@@ -0,0 +1,30 @@
+/**
+Serve the files in the "starting-files" folder
+To get your images to serve, use:
+	func StripPrefix(prefix string, h Handler) Handler
+	func FileServer(root FileSystem) Handler
+Constraint: you are not allowed to change the route being used for images in the template file
+**/
+
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, _ *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
